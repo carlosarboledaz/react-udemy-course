ed65076bc01a86de5490c9cb9f1c56a49c1ffb27
Carlos Arboleda <carlos.arboleda@perficient.com>
2022-08-24 21:57:28 -0500

    Adding a re-usable button component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import Card from '../UI/Card';
-import classes from './AddUser.module.css';
+import React from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
 
-const AddUser = props => {
+const AddUser = (props) => {
+  const AddUserHandler = (event) => {
+    event.preventDefault();
+  };
 
-    const AddUserHandler = (event) => {
-        event.preventDefault();
-    }
-
-    return (
-        <Card className={classes.input}>
-            <form onSubmit={AddUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text"></input>
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number"></input>
-                <button type="submit">Add User</button>
-            </form>
-        </Card>
-        
-    )
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
